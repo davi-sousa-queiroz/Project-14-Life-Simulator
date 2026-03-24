a6011716398d0cf44b426c51fd81a64da82bb8a8
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-24 15:20:05 -0300

    Add main player atributes

---
+++ Life Sim.py
@@ -0,0 +1,10 @@
+class Player:
+
+    def __init__(self, name, last_name):
+        self.name = name
+        self.last_name = last_name
+        self.age = 16
+        self.money = 100
+        self.energy = 100
+
+        
