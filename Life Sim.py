de5f70464a422b1720e37aab383eda219001fdc9
Davi Queiroz <daviqueiroz26@proton.me>
2026-03-24 15:20:32 -0300

    add show stats method

--- Life Sim.py
+++ Life Sim.py
@@ -7,4 +7,12 @@ class Player:
         self.money = 100
         self.energy = 100
 
-        
+    def show_stats(self):
+        stats = {"First Name": self.name,
+                 "Last Name": self.last_name,
+                 "Age": self.age,
+                 "Money": self.money,
+                 "Energy": self.energy}
+
+        for key, value in stats.items():
+            print(f"{key}: {value}")
